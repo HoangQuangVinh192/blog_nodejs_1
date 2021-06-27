e4458ebd8eb45088be390d0e79941e72a2b93f83
Vinh Hoàng Quang <hvinh06@gmail.com>
2021-06-27 22:27:49 +0700

    connect to db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,9 +1,17 @@
-
+const Course = require('../models/Course');
 class SiteController {
-
     // [GET] /
     index(req, res) {
-        res.render('home');
+
+        Course.find({}, function (err, courses) {
+            if(!err) {
+               res.json(courses); 
+            } else {
+               res.status(400).json({ error: 'ERROR!!!' }); 
+            }
+        });
+
+        // res.render('home');
     }
 
     // [GET] /search
@@ -12,4 +20,4 @@ class SiteController {
     }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
---
+++ src/config/db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+
+    try {
+        await mongoose.connect('mongodb://localhost:27017/example_dev', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+        });
+        console.log('Connect to DB successfully!!!!!!!!');
+    } catch (error) {
+        console.log('Connect to DB failure!!!!!!!!!!!!');
+    }
+}
+
+module.exports = { connect };
